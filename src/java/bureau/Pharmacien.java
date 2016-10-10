8340ad9050fe11170ee9345d13f72e559205a3ed
aboura <aboura@ETUDIANTS8-9ECP.castres.univ-jfc.fr>
2016-10-10 12:13:50 +0200

    Merge tout

---
+++ src/java/bureau/Pharmacien.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bureau;
+
+/**
+ *
+ * @author aboura
+ */
+public class Pharmacien {
+
+    int id_boss;
+    String nom;
+    String prenom;
+
+    public Pharmacien(int id_boss, String nom, String prenom) {
+        this.id_boss = id_boss;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public int getId_boss() {
+        return id_boss;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setId_boss(int id_boss) {
+        this.id_boss = id_boss;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+}

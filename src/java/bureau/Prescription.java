c2ad6737c0b4fd93cac9d543df92028ba7c37d31
aboura <aboura@ETUDIANTS8-9ECP.castres.univ-jfc.fr>
2016-10-10 12:13:56 +0200

    Merge tout

---
+++ src/java/bureau/Prescription.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bureau;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aboura
+ */
+public class Prescription {
+
+    int id_pres;
+    Date date_press;
+    Avancement avancement;
+    int cout_total;
+
+    public Prescription(int id_pres, Date date_press, Avancement avancement, int cout_total) {
+        this.id_pres = id_pres;
+        this.date_press = date_press;
+        this.avancement = avancement;
+        this.cout_total = cout_total;
+    }
+
+    public int getId_pres() {
+        return id_pres;
+    }
+
+    public Date getDate_press() {
+        return date_press;
+    }
+
+    public Avancement getAvancement() {
+        return avancement;
+    }
+
+    public int getCout_total() {
+        return cout_total;
+    }
+
+    public void setId_pres(int id_pres) {
+        this.id_pres = id_pres;
+    }
+
+    public void setDate_press(Date date_press) {
+        this.date_press = date_press;
+    }
+
+    public void setAvancement(Avancement avancement) {
+        this.avancement = avancement;
+    }
+
+    public void setCout_total(int cout_total) {
+        this.cout_total = cout_total;
+    }
+
+}

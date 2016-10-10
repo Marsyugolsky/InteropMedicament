7be3c831fc4579feec45107e636a93b297fb2fe4
aboura <aboura@ETUDIANTS8-9ECP.castres.univ-jfc.fr>
2016-10-10 11:48:25 +0200

    Pharmacie, Enum, Prescription + UF, admission + Administration

---
+++ src/java/bureau/Administration.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bureau;
+
+/**
+ *
+ * @author aboura
+ */
+public enum Administration {
+    epidermique,
+    dermique,
+    auriculaire,
+    nasale,
+    pulmonaire,
+    vaginale,
+    oculaire,
+    intravesicale,
+    buccale,
+    orale,
+    rectale;
+}

3269a7ee80d6bf7a11896ed72e5566660aa551dc
mgros <mgros@ETUDIANTS8-8E4P.castres.univ-jfc.fr>
2016-10-19 12:10:05 +0200

    création de propose stock et fournis. Mise à jour de la méthode ajouterMedicament

---
+++ src/java/bureau/Propose.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bureau;
+
+/**
+ *
+ * @author mgros
+ */
+public class Propose {
+     Medicament med;
+    int quantite;
+
+    public Propose(Medicament med, int quantite) {
+        this.med = med;
+        this.quantite = quantite;
+    }
+
+    public Medicament getMed() {
+        return med;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setMed(Medicament med) {
+        this.med = med;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+}
